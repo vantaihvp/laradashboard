a6ff9e6b6a09bc2ec421139263684578f15d23ec
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-04-19 13:31:47 +0600

    Fixed links

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -19,9 +19,4 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
-
-    public function forgotpasswordpage()
-    {
-        $this->showLinkRequestForm();
-    }
 }
