fa0cba05434258cddc0338c211c0c2a34b332ef1
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-04-19 14:52:40 +0600

    Fixed forget password success method

--- app/Http/Controllers/Backend/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Backend/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +29,17 @@ class ForgotPasswordController extends Controller
     public function showLinkRequestForm()
     {
         return view('backend.auth.passwords.email');
+    }
+
+    /**
+     * Get the response for a successful password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return back()->with('success', trans($response));
     }
 }
