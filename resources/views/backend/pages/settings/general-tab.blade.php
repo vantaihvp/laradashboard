23f1a5b8a2f604360284b5d175ae6fd9a90ea849
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-04-20 16:00:01 +0600

    Role List/Create/Edit UI improvement with translations

--- resources/views/backend/pages/settings/general-tab.blade.php
+++ resources/views/backend/pages/settings/general-tab.blade.php
@@ -5,7 +5,6 @@
         </h3>
     </div>
     <div class="space-y-6 border-t border-gray-100 p-5 sm:p-6 dark:border-gray-800">
-
         <div class="flex">
             <div class="md:basis-1/2 relative">
                 <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
