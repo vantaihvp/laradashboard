48660a4a11e9d0eeaa8a8a67c89892c9b31a2d74
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-05-11 15:44:06 +0600

    Cleanup demo service with login request

---
+++ app/Services/DemoAppService.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+/**
+ * This class is for demo app.
+ * 
+ * This class method will only be executed if the demo app is enabled in the .env file.
+ *
+ * Before every method, it will check if the demo app is enabled.
+ */
+class DemoAppService
+{
+    public function isDemoAppEnabled(): bool
+    {
+        return env('DEMO_MODE', false);
+    }
+
+    public function getDemoAppUrl(): string
+    {
+        return env('DEMO_APP_URL', 'https://demo.laradashboard.com');
+    }
+
+    public function maybeSetDemoLocaleToEnByDefault(): void
+    {
+        if ($this->isDemoAppEnabled()) {
+            app()->setLocale('en');
+        }
+    }
+}
