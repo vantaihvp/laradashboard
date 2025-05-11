607d0aa662a8605ad359964d6434fb8a5a6f517f
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-05-12 02:22:47 +0600

    Run console in every 15 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,8 +23,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // Schedule the demo database refresh command every hour in demo mode.
-        $schedule->command('demo:refresh-database')->hourly();
+        // Schedule the demo database refresh command every 15 minutes in demo mode.
+        $schedule->command('demo:refresh-database')->everyFifteenMinutes();
     }
 
     /**
