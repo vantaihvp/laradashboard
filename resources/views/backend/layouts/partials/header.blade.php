6959d6d3284a730cb81aaabffcf9fd6ecffc9259
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-05-11 11:21:01 +0600

    Removed z-idnex and enabled default modules

--- resources/views/backend/layouts/partials/header.blade.php
+++ resources/views/backend/layouts/partials/header.blade.php
@@ -27,7 +27,7 @@ x-data="{
     }
 }"
 x-init="init()"
-    class="sticky top-0 flex w-full border-gray-200 lg:border-b dark:border-gray-800 z-[9999]">
+    class="sticky top-0 flex w-full border-gray-200 lg:border-b dark:border-gray-800">
     <div class="flex grow flex-col items-center justify-between lg:flex-row lg:px-6">
         <div
             class="flex w-full items-center justify-between gap-2 border-b border-gray-200 px-3 py-3 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4 dark:border-gray-800">
