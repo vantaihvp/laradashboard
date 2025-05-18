faf18c149cfc3c3bfd178c1311e292ca2681a227
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-05-18 10:43:06 +0600

    Slugify group name

--- resources/views/backend/layouts/partials/sidebar-menu.blade.php
+++ resources/views/backend/layouts/partials/sidebar-menu.blade.php
@@ -23,19 +23,19 @@
     class="transition-all duration-300 ease-in-out"
 >
     @foreach($menuGroups as $groupName => $groupItems)
-        {!! ld_apply_filters('sidebar_menu_group_before_' . strtolower($groupName), '') !!}
+        {!! ld_apply_filters('sidebar_menu_group_before_' . Str::slug($groupName), '') !!}
         <div>
-            {!! ld_apply_filters('sidebar_menu_group_heading_before_' . strtolower($groupName), '') !!}
+            {!! ld_apply_filters('sidebar_menu_group_heading_before_' . Str::slug($groupName), '') !!}
             <h3 class="mb-4 text-xs uppercase leading-[20px] text-gray-400 px-5">
                 {{ __($groupName) }}
             </h3>
-            {!! ld_apply_filters('sidebar_menu_group_heading_after_' . strtolower($groupName), '') !!}
+            {!! ld_apply_filters('sidebar_menu_group_heading_after_' . Str::slug($groupName), '') !!}
             <ul class="flex flex-col mb-6">
-                {!! ld_apply_filters('sidebar_menu_before_all_' . strtolower($groupName), '') !!}
+                {!! ld_apply_filters('sidebar_menu_before_all_' . Str::slug($groupName), '') !!}
                 {!! $menuService->render($groupItems) !!}
-                {!! ld_apply_filters('sidebar_menu_after_all_' . strtolower($groupName), '') !!}
+                {!! ld_apply_filters('sidebar_menu_after_all_' . Str::slug($groupName), '') !!}
             </ul>
         </div>
-        {!! ld_apply_filters('sidebar_menu_group_after_' . strtolower($groupName), '') !!}
+        {!! ld_apply_filters('sidebar_menu_group_after_' . Str::slug($groupName), '') !!}
     @endforeach
 </nav>
