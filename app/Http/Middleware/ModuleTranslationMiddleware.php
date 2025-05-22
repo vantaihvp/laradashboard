cc30f667b3935d17309b6384bbaa71798cd40cd9
engrshishir <shishir.cse.pstu@gmail.com>
2025-05-22 23:20:33 +0600

    load module-wise JSON translations based on session locale

---
+++ app/Http/Middleware/ModuleTranslationMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Session;
+use Nwidart\Modules\Facades\Module;
+
+class ModuleTranslationMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $locale = Session::get('locale', config('app.locale'));
+        App::setLocale($locale);
+        
+        foreach (Module::all() as $module) {
+            $jsonPath = $module->getPath() . "/Resources/lang/{$locale}.json";
+
+            if (File::exists($jsonPath)) {
+                $translations = json_decode(File::get($jsonPath), true);
+
+                if (is_array($translations)) {
+                    app('translator')->addLines($translations, $locale);
+                }
+            }
+        }
+
+        return $next($request);
+    }
+}
